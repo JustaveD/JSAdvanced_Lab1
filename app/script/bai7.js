2737394ca3a6ccc6d3abf85bbc5f653c65985ae8
JustaveD <yeumytoan0101@gmail.com>
2022-01-05 20:25:23 +0700

    them su kien blur ra thi dung dem so

--- app/script/bai7.js
+++ app/script/bai7.js
@@ -3,17 +3,43 @@ const button = document.querySelector('.card__button');
 const download = document.querySelector(".btn-download");
 
 let count = 60;
-let counter = setInterval(timer, 1000);
+let counter;
+window.onload = function () {
+    console.log('onload');
+    counter = setInterval(timer, 1000);
+    function timer() {
+        count -= 1;
+        if (count === -1) {
+            clearInterval(counter);
+            download.classList.add("active");
+            return;
+        }
 
-function timer() {
-    count -= 1;
-    if (count === -1) {
-        clearInterval(counter);
-        download.classList.add("active");
-        return;
+        document.getElementById("num").innerHTML = count;
     }
 
-    document.getElementById("num").innerHTML = count;
 }
 
+window.addEventListener('focus', function () {
+    console.log('focus');
+    counter = setInterval(timer, 1000);
+    function timer() {
+        count -= 1;
+        if (count === -1) {
+            clearInterval(counter);
+            download.classList.add("active");
+            return;
+        }
 
+        document.getElementById("num").innerHTML = count;
+    }
+
+})
+
+
+
+
+window.addEventListener('blur', function () {
+    console.log('blur');
+    clearInterval(counter);
+})
